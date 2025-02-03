cc286b8d23b6e4bf038ea490a37f355a80dc8ddf
Artur <proxaker1111@gmail.com>
2025-02-03 20:20:38 +0200

    stepik course 2

---
+++ hr/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Employee(models.Model):
+    first_name = models.CharField(max_length = 150)
+    last_name = models.CharField(max_length=150)
+    class Meta:
+        db_table = "employe"
+    

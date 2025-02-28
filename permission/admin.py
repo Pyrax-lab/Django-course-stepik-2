6347b461b49a31bbe864149613e833fc82ba76df
Artur <proxaker1111@gmail.com>
2025-02-28 14:21:55 +0200

    django-chanels

--- permission/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from permission.models import Post 
-from guardian.admin import GuardedModelAdmin
-# Register your models here.
-
-class PostAdmin(GuardedModelAdmin):
-    list_display = ('title', )
-
-admin.site.register(Post, PostAdmin)

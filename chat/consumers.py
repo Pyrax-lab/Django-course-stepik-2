3dddcae2a25dbd000b0d88ef5c135bbe569d8d04
Artur <proxaker1111@gmail.com>
2025-03-05 23:52:53 +0200

    webcocket more info

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,17 +1,36 @@
-from channels.generic.websocket import WebsocketConsumer, AsyncWebsocketConsumer
+
+# # consumers - это аналог view только для сокетов
+
+# from channels.generic.websocket import WebsocketConsumer, AsyncWebsocketConsumer
+
+# class JoinAndLeave(WebsocketConsumer):
+
+#     # Вызывается когад клиент пытается установить соединение здесь пишем логику присоединения 
+#     def connect(self):
+#         print("Server say connect")
+#         # должны обязательно вызвать accept() чтоб подтвердить соединение 
+#         self.accept()
+
+#     # Этот метод вызывается всякий раз, когда сервер получает сообщение от клиента через WebSocket. 
+#     def receive(self, text_data=None, bytes_data=None):
+#         print("Server says client message reiceved: ", text_data)
+#         self.send("Server sends Welcome ") # метод send() отправляет ответ клиенту 
+
+#     # Этот метод вызывается, когда WebSocket-соединение закрывается. Это может быть связано с тем, что клиент закрыл соединение или произошла ошибка.
+#     def disconnect(self, close):
+#         print("Server says disconnect")
+
+
+
+from channels.generic.websocket import WebsocketConsumer
 
 class JoinAndLeave(WebsocketConsumer):
 
-    # Вызывается когад клиент пытается установить соединение здесь пишем логику присоединения 
     def connect(self):
-        print("Server say connect")
-        # должны обязательно вызвать accept() чтоб подтвердить соединение 
+        
+        self.accept()
+        self.send("Привет клиент")
 
-    # Этот метод вызывается всякий раз, когда сервер получает сообщение от клиента через WebSocket. 
-    def receive(self, text_data=None, bytes_data=None):
-        print("Server says client message reiceved: ", text_data)
-        self.send("Server sends Welcome ") # метод send() отправляет ответ клиенту 
-
-    # Этот метод вызывается, когда WebSocket-соединение закрывается. Это может быть связано с тем, что клиент закрыл соединение или произошла ошибка.
-    def disconnect(self, close):
-        print("Server says disconnect")
+    def receive(self, text_data = None, bytes_data = None):
+        print(text_data)
+        
--- django_chat/asgi.py
+++ django_chat/asgi.py
@@ -8,15 +8,20 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
-from channels.routing import ProtocolTypeRouter, URLRouter 
 from django.core.asgi import get_asgi_application
 import chat.routing
 
+from channels.routing import ProtocolTypeRouter, URLRouter 
+from channels.security.websocket import AllowedHostsOriginValidator # Блокирует WebSocket-подключения, которые приходят с доменов, не указанных в ALLOWED_HOSTS Django, защита от cors-атак
+from channels.auth import AuthMiddlewareStack # Добавляет механизм аутентификации позволяет работать с пользователем self.scope['user'] аналог request.user
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_chat.settings')
 
 asgi_application = get_asgi_application()
 
 application = ProtocolTypeRouter({
-    "http": asgi_application,
-    "websocket": URLRouter(chat.routing.websocket_urlpatterns)
+    "http": asgi_application,# это наши стандартные url-ы 
+    "websocket": AllowedHostsOriginValidator( # Защита от атак
+                 AuthMiddlewareStack(
+                 URLRouter(chat.routing.websocket_urlpatterns)))
+    #"websocket": URLRouter(chat.routing.websocket_urlpatterns) самый просто способ добавление маршрутов для вебсокетов
 })

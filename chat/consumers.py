3c002858b35995054d6eeb4cfd611263a294298b
Artur <proxaker1111@gmail.com>
2025-03-12 20:26:40 +0200

    Fatal error with groups and uuid

--- chat/consumers.py
+++ chat/consumers.py
@@ -22,8 +22,8 @@
 
 
 # Простой сокет отправляющий 99 чисел клиенту
-from channels.generic.websocket import WebsocketConsumer, AsyncWebsocketConsumer
-import time, asyncio
+# from channels.generic.websocket import WebsocketConsumer, AsyncWebsocketConsumer
+# import time, asyncio
 
 # class JoinAndLeave(AsyncWebsocketConsumer):
 
@@ -33,10 +33,47 @@ import time, asyncio
 #             await self.send(f"Клиент держи число {i}")
 #             await asyncio.sleep(1)
 
-class JoinAndLeave(AsyncWebsocketConsumer):
 
-    async def connect(self):
+import json, asyncio
+from channels.generic.websocket import WebsocketConsumer, AsyncWebsocketConsumer 
+from accounts.models import User 
+from .models import Group, Message, Event 
+
+class JoinAndLeave(WebsocketConsumer):
+    
+    def connect(self):
+        self.user = self.scope['user']
+        self.accept()
+
+    def receive(self, text_data=None, bytes_data=None):
+        text_data = json.loads(text_data) # json словарь с ответам от сервера 
+        type = text_data.get('type', None)
         
-        print("!!!!!!!!!!!!")
-        print(self.scope["user"])
-        await self.accept()
+        if type:
+            data_uuid = text_data.get('data', None)
+        if type == 'leave_group':
+            self.leave_group(data_uuid)
+            print(type)
+        if type == "join_group":
+            self.join_group(data_uuid)
+    
+    def leave_group(self, data_uuid):
+        print(data_uuid)
+        group = Group.objects.get(uuid = data_uuid)
+        group.remove_user_from_group(self.user)
+        data = {
+            'type': 'leave_group',
+            'data': data_uuid,
+        }
+        print("sssss")
+        self.send(json.dumps(data))
+
+    def join_group(self, data_uuid):
+        group = Group.objects.get(uuid = data_uuid)
+        group.add_user_to_group(self.user)
+        data = {
+            'type' : 'join_group',
+            'data' : data_uuid,
+        }
+        self.send(json.dumps(data))
+

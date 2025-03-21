f25b0a929940cb1da138cefc1ae7dc38beef65cd
Artur <proxaker1111@gmail.com>
2025-03-21 19:16:11 +0200

    django-celery

--- htmx/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from htmx.models import Books 
-# Register your models here.
-
-@admin.register(Books)
-class AdminBooks(admin.ModelAdmin):
-    list_display = ('title', 'author', 'price', 'read')
-    
